2176570e31bd1aaeb9b5170102320a2a325daa15
neDmitry2 <verygadrush@gmail.com>
2025-09-12 10:40:59 +0700

    Add main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import TrackerControls from '@/components/controls';
+import MapDisplay from '@/components/map';
+import { LocationObjectCoords } from 'expo-location';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 export default function Index() {
+  const [isTracking, setIsTracking] = useState(false);
+  const [route, setRoute] = useState<LocationObjectCoords[]>([]);
+
+  const handleStart = () => {
+    console.log("Start tracking...");
+    setIsTracking(true);
+  };
+
+  const handleStop = () => {
+    console.log("Stop tracking...");
+    setIsTracking(false);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <MapDisplay route={route} />
+      <TrackerControls 
+        isTracking={isTracking}
+        onStart={handleStart}
+        onStop={handleStop}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

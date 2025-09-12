fd5c9382e4e2ecf5c51f5e6e16d9ac4cffd7397e
neDmitry2 <verygadrush@gmail.com>
2025-09-12 10:15:42 +0700

    Create map.tsx

---
+++ components/map.tsx
@@ -0,0 +1,39 @@
+import { LocationObjectCoords } from 'expo-location';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
+
+interface MapDisplayProps {
+  route: LocationObjectCoords[];
+}
+
+export default function MapDisplay({ route }: MapDisplayProps) {
+  return (
+    <MapView
+      style={styles.map}
+      showsUserLocation
+      followsUserLocation
+      initialRegion={{
+        latitude: 56.01839,
+        longitude: 92.86717,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.05,
+      }}
+    >
+      {route.length > 1 && (
+        <Polyline
+          coordinates={route}
+          strokeColor="#FF0000" // Red
+          strokeWidth={6}
+        />
+      )}
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
